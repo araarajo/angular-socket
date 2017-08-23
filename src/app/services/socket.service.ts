b77fb58a772fcb330e049c7608efed8b2b6975b9
Ara <elishaara@gmail.com>
2017-08-23 11:12:17 +0900

    add socket service

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 @Injectable()
 export class SocketService {
+  private subject$: Subject<MessageEvent>;
+  private url: string;
+  public close: Function;
+  public getState: Function;
 
-  constructor() { }
+  constructor() {
+    this.url = 'http://localhost:3000';
+  }
+
+  /**
+   *
+   * @returns {Subject<MessageEvent>}
+   */
+  connect(): Subject<MessageEvent> {
+    this.subject$ = this.create();
+    return this.subject$
+  }
+
+  private create(): Subject<MessageEvent> {
+    const ws = new WebSocket(this.url);
+
+    const observable$ = Observable.create((obs: Observer<MessageEvent>) => {
+      ws.onopen = obs.next.bind(obs);
+      ws.onmessage = obs.next.bind(obs);
+      ws.onclose = obs.complete.bind(obs);
+      ws.onerror = obs.error.bind(obs);
+      return ws.close.bind(ws);
+    });
+
+    const observer = {
+      next: (data: string) => {
+        if ( ws.readyState === WebSocket.OPEN ) {
+          ws.send(data);
+        }
+      }
+    };
+
+    const getState = () => {
+      return ws.readyState;
+    };
+
+    this.close = ws.close.bind(ws);
+    this.getState = getState.bind(ws);
+
+    return Subject.create(observer, observable$);
+  }
 
 }
